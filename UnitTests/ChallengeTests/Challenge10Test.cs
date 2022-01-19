621ca57e097e66248050203f52039162edcb88a2
Liam Ward <liam.ward@unifysolutions.net>
2022-01-20 07:53:16 +1000

    Added Challenge 10 and 11

---
+++ UnitTests/ChallengeTests/Challenge10Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Challenges;
+
+namespace ChallengeTests
+{
+    [TestFixture]
+    class Challenge10Test
+    {
+        [Test]
+        public void SumOfPrimesBelow10()
+        {
+            Challenge10 challenge10 = new Challenge10();
+            challenge10._CountUpTo = 10;
+            Assert.AreEqual(17, challenge10.RunChallenge());
+        }
+
+        [Test]
+        public void SumOfPrimesBelow2Million()
+        {
+            Challenge10 challenge10 = new Challenge10();
+            challenge10._CountUpTo = 2000000;
+            Assert.AreEqual(142913828922, challenge10.RunChallenge());
+        }
+
+
+    }
+}
---
+++ UnitTests/ChallengeTests/Challenge11Test.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Challenges;
+
+namespace ChallengeTests
+{
+    [TestFixture]
+    class Challenge11Test
+    {
+        [Test]
+        public void Test4Adjacent()
+        {
+            Challenge11 challenge11 = new Challenge11();
+            challenge11._AdjacentsToFind = 4;
+            Assert.AreEqual(1788696, challenge11.RunChallenge());
+        }
+    }
+}
