99d3322c90df80c2241bfa4c1995d58c534aefe9
Liam Ward <liamtgward@gmail.com>
2022-01-30 19:34:20 +1000

    Amicable

--- Enumerators/AmicableNumbers.cs
+++ Enumerators/AmicableNumbers.cs
@@ -3,27 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Enumerators;
+using Functions;
 
 namespace ProjectEuler.Enumerators
 {
     public static class AmicableNumbers
     {
-        public static IEnumerable<long[]> Sequence(this long calculateUntil)
+        public static List<long[]> List(this long calculateUntil)
         {
             //count up to Calculate until
             //Get a list of all divisors, and sum them
             //Check 
 
-            do
-            { 
-                long[] AmicablePair;
+            //Generate 2 lists of natural numbers up to the calculate until
+            //find some way to verify if numbers are Amicable
+            NaturalNumbers natural = new NaturalNumbers();
+
+            var seedSetA = natural.Take((int)calculateUntil);
+            var seedSetB = natural.Take((int)calculateUntil);
 
 
+            List<long[]> PairsOfAmicableNumbers = new List<long[]>();
 
-                //yield return AmicablePair;
-                //AmicablePair.
 
-            } while (true);
+            foreach (var seedNumberA in seedSetA)
+            {
+                foreach (var seedNumberB in seedSetB)
+                {
+                    if (IsAmicable(seedNumberA, seedNumberB))
+                    {
+                        PairsOfAmicableNumbers.Add(new[] {seedNumberA, seedNumberB});
+                    }
+
+                }
+
+            }
+
+            return PairsOfAmicableNumbers;
+        }
+
+        public static bool IsAmicable(long A, long B)
+        {
+            var SumOfAllMultiplesOfA = Divisors.List(A).SkipLast(1).Sum();
+            var SumOfAllMultiplesOfB = Divisors.List(B).SkipLast(1).Sum();
+
+            if (SumOfAllMultiplesOfA == B && SumOfAllMultiplesOfB == A)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
