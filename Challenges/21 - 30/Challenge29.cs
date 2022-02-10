94ea0f182eb0be7d999f3fc8f10021738567b02e
Liam Ward <liamtgward@gmail.com>
2022-02-11 08:05:13 +1000

    Resolve Conflicts

---
+++ Challenges/21 - 30/Challenge29.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using ProjectEuler;
+
+namespace Challenges21To30
+{
+    public class Challenge29 : IRunChallenge
+    {
+
+        public long Ceiling;
+
+        public long RunChallenge()
+        {
+            List<BigInteger> allProducts = new List<BigInteger>();
+            //Just 2 loops?
+            for (int a = 2; a <= Ceiling; a++)
+            {
+                for (int b = 2; b <= Ceiling; b++)
+                {
+                   
+                    allProducts.Add((BigInteger)Math.Pow(a,b));
+
+                }
+            }
+
+            var distinct = allProducts.Distinct();
+
+
+
+
+            return distinct.Count();
+        }
+
+
+    }
+}
---
+++ UnitTests/ChallengeTests/21 - 30/Challenge29Test.cs
@@ -0,0 +1,30 @@
+﻿using Challenges;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges21To30
+{
+    [TestFixture]
+    class Challenge29Test
+    {
+        [Test]
+        public void OneHundred()
+        {
+            Challenge29 challenge = new Challenge29();
+            challenge.Ceiling = 100;
+            Assert.AreEqual(9183, challenge.RunChallenge());
+        }
+
+        [Test]
+        public void Five()
+        {
+            Challenge29 challenge = new Challenge29();
+            challenge.Ceiling = 5;
+            Assert.AreEqual(15, challenge.RunChallenge());
+        }
+    }
+}
